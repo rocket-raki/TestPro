015cb9ca6a6d9f67c42a9e7da68e7baa02fe0b0f
Rakesh D <157192623+rocket-raki@users.noreply.github.com>
2025-06-29 23:52:54 +0530

    Update settings.py

--- testpro/testpro/settings.py
+++ testpro/testpro/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-c)2u$0k^!7h_2)1)h2iz&yikphc6i5)f=whr)baun^*wzlt#jw
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
